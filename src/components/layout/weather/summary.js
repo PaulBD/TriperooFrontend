1f39e26d849cf9dcb7fd3dbd7e44ffca6f21a20e
Paul Billington-Dykes <paul.billingtondykes@gmail.com>
2017-12-12 12:50:22 +0000

    Updated to fix summary bug

--- src/components/layout/weather/summary.js
+++ src/components/layout/weather/summary.js
@@ -4,60 +4,59 @@ import {bindActionCreators} from 'redux';
 import * as weatherActions from '../../../actions/location/common/weatherActions';
 
 class Summary extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-	}
+  constructor(props, context) {
+    super(props, context);
+  }
 
-	componentDidMount() {
-		this.props.actions.loadCurrentWeather(this.props.locationId, true, 'en');
-	}
+  componentDidMount() {
+    this.props.actions.loadCurrentWeather(this.props.locationId, true, 'en');
+  }
 
-	render()
-	{
-		if ((this.props.weather != undefined) && (this.props.weather.currently != undefined))
-		{
-		  console.log(this.props.weather.currently);
-			let imageUrl = "/static/img/weather-icons/" + this.props.weather.currently.icon + ".png";
+  render()
+  {
+    if ((this.props.weather != undefined) && (this.props.weather.currently != undefined))
+    {
+      let imageUrl = "/static/img/weather-icons/" + this.props.weather.currently.icon + ".png";
 
-			return (
-				<div className="owl-cap-weather">
+      return (
+        <div className="owl-cap-weather">
           <img src={imageUrl} width="32" height="32" className="weatherIcon" />&nbsp;
-					<span className="icon">{Math.round(this.props.weather.currently.temperature)}</span>
+          <span className="icon">{Math.round(this.props.weather.currently.temperature)}</span>
           <span>{this.props.weather.currently.summary}</span>
-				</div>
-			);
-		}
-		else {
-			return (<div className="owl-cap-weather">&nbsp;</div>);
-		}
-	}
+        </div>
+      );
+    }
+    else {
+      return (<div className="owl-cap-weather">&nbsp;</div>);
+    }
+  }
 }
 
 
 Summary.defaultProps = {
-	weather: {},
-	isFetching: false
+  weather: {},
+  isFetching: false
 };
 
 Summary.propTypes = {
-	locationId: PropTypes.number.isRequired,
-	type: PropTypes.string.isRequired,
-	weather: PropTypes.object.isRequired,
-	actions: PropTypes.object.isRequired,
-	isFetching: PropTypes.bool.isRequired
+  locationId: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  weather: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
+  isFetching: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-	return {
-		isFetching: state.weather.isFetching ? state.weather.isFetching : false,
-		weather: state.weather.weather ? state.weather.weather : {}
-	};
+  return {
+    isFetching: state.weather.isFetching ? state.weather.isFetching : false,
+    weather: state.weather.weather ? state.weather.weather : {}
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(weatherActions, dispatch)
-	};
+  return {
+    actions: bindActionCreators(weatherActions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Summary);
